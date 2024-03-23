5b66b97f0d2320e2b0439f0b209bfa931d4abddb
rysaev <rysaev@socit.ru>
2024-03-23 10:35:25 +0500

    4.6. Беда в колл-центре

---
+++ Courses/Course84983/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество строк n");
+        int n = Console.Read();
+        string[] Array_Str = new string[n];
+        Console.WriteLine("Введите строку ");
+        //...
+        Console.ReadKey();
+    }
+}
